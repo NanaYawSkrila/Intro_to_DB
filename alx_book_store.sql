813c190165253876b7912a5087957b960026225a
NanaYawSkrila <141090270+NanaYawSkrila@users.noreply.github.com>
2025-10-03 13:05:29 +0000

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,12 +1,11 @@
 -- FILE: alx_book_store.sql
 -- STEP 1: CREATE DATABASE AND SELECT IT
 
-CREATE DATABASE IF NOT EXISTS `alx_book_store`
+CREATE DATABASE IF NOT EXISTS alx_book_store
 CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
-USE `alx_book_store`;
-
+USE alx_book_store;
 
 
 -- STEP 3: CREATE TABLES
