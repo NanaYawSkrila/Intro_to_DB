c003b5efede86f400bd9ec500b75eed5a750ab9e
NanaYawSkrila <skriladrilladerula@icloud.com>
2025-10-03 12:57:01 +0000

    Step 3: Add tables Authors, Books, Customers, Orders, Order_Details

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -6,3 +6,49 @@ CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
 USE `alx_book_store`;
+
+
+
+-- STEP 3: CREATE TABLES
+
+-- 1. Authors table
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- 2. Books table
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- 3. Customers table
+CREATE TABLE Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE,
+    address TEXT
+);
+
+-- 4. Orders table
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- 5. Order_Details table
+CREATE TABLE Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
