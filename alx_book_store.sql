17f3fd9c6e729a2e7067d00f64ce3ff341b8b02f
NanaYawSkrila <skriladrilladerula@icloud.com>
2025-10-03 12:43:50 +0000

    Add alx_book_store.sql — create database and USE statement (Step 1)

---
+++ alx_book_store.sql
@@ -0,0 +1,8 @@
+-- FILE: alx_book_store.sql
+-- STEP 1: CREATE DATABASE AND SELECT IT
+
+CREATE DATABASE IF NOT EXISTS `alx_book_store`
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+USE `alx_book_store`;
