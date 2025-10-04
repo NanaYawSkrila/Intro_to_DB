d6496ec274a8e410364b04611c8c8cd27ab006c5
NanaYawSkrila <skriladrilladerula@icloud.com>
2025-10-04 07:29:00 +0000

    Task 5: Insert first customer record into Customers table

---
+++ task_5.sql
@@ -0,0 +1,7 @@
+-- FILE: task_5.sql
+-- STEP 5: INSERT A SINGLE ROW INTO THE CUSTOMERS TABLE
+
+USE alx_book_store;
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
