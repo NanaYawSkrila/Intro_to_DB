cc254a08cd764bbfbfe5fa740491d3d3300fa56d
NanaYawSkrila <skriladrilladerula@icloud.com>
2025-10-04 07:56:05 +0000

    Task 5: Insert single row into Customers table

--- task_5.sql
+++ task_5.sql
@@ -1,7 +1,7 @@
 -- FILE: task_5.sql
--- STEP 5: INSERT A SINGLE ROW INTO THE CUSTOMERS TABLE
+INSERT INTO Customers (customer_name, email, address)
+VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
+ON DUPLICATE KEY UPDATE
+customer_name = VALUES(customer_name),
+address = VALUES(address);
 
-USE alx_book_store;
-
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
