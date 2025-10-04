04febc30becd320315f7af30e1bdbe0cded2f606
NanaYawSkrila <skriladrilladerula@icloud.com>
2025-10-04 08:10:52 +0000

    Add task_5.sql — insert first record into customers table

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,8 @@
--- FILE: task_5.sql
-INSERT INTO Customers (customer_name, email, address)
-VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
+-- FILE: task_5
+USE alx_book_store;
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 ON DUPLICATE KEY UPDATE
 customer_name = VALUES(customer_name),
 address = VALUES(address);
